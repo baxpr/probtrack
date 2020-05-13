b851ff4d256beefcce5c393beb9bd139262f5421
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-13 16:33:01 -0500

    remove split/join functions

--- src/functions.sh
+++ src/functions.sh
@@ -3,31 +3,6 @@
 # Support functions
 
 
-# Split a single ROI mask from a multi-ROI image
-function split_roi () {
-	local in_niigz="${1}"
-	local val="${2}"
-	
-	out_niigz=$( basename "${in_niigz}" .nii.gz )_"${val}"
-	fslmaths "${in_niigz}" -thr "${val}" -uthr "${val}" -bin "${out_niigz}"
-}
-
-
-# Join multiple ROI masks into a single one
-function join_rois () {
-	local in_niigz="${1}"
-	local out_niigz="${2}"
-	local vals="${3}"
-	
-	addstr=""
-	for v in $vals ; do
-		fstr=$( basename ${in_niigz} .nii.gz)_"${v}"
-		addstr="${addstr} -add ${fstr}"
-	done
-	fslmaths "${in_niigz}" -thr 0 -uthr 0 ${addstr} -bin "${out_niigz}"
-}
-
-
 # Join multiple ROI masks into a single one
 function combine_rois () {
 	local in_niigz="${1}"
