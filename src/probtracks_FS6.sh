10d6c34978d12fd775c760f15797fd88d7bd865b
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-13 21:03:29 -0500

    biggest

--- src/probtracks_FS6.sh
+++ src/probtracks_FS6.sh
@@ -57,3 +57,41 @@ probtrackx2 \
 	${trackopts}
 cp "${targets_dir}"/TARGETS_FS6_R.txt "${track_dir}"/FS_THALAMUS_R_to_TARGETS_R
 
+
+
+# Segmentation
+cd "${track_dir}"
+mkdir BIGGEST
+for LR in L R ; do
+
+	find_the_biggest \
+		FS_THALAMUS_${LR}_to_FS_PFC_${LR}/seeds_to_FS_PFC_${LR} \
+		FS_THALAMUS_${LR}_to_FS_MOTOR_${LR}/seeds_to_FS_MOTOR_${LR} \
+		FS_THALAMUS_${LR}_to_FS_SOMATO_${LR}/seeds_to_FS_SOMATO_${LR} \
+		FS_THALAMUS_${LR}_to_FS_POSTPAR_${LR}/seeds_to_FS_POSTPAR_${LR} \
+		FS_THALAMUS_${LR}_to_FS_OCC_${LR}/seeds_to_FS_OCC_${LR} \
+		FS_THALAMUS_${LR}_to_FS_TEMP_${LR}/seeds_to_FS_TEMP_${LR} \
+		BIGGEST/seg_all_${LR}
+		
+	fslmaths BIGGEST/seg_all_${LR} -thr 1 -uthr 1 -bin BIGGEST/seg_FS_PFC_${LR}
+	fslmaths BIGGEST/seg_all_${LR} -thr 2 -uthr 2 -bin BIGGEST/seg_FS_MOTOR_${LR}
+	fslmaths BIGGEST/seg_all_${LR} -thr 3 -uthr 3 -bin BIGGEST/seg_FS_SOMATO_${LR}
+	fslmaths BIGGEST/seg_all_${LR} -thr 4 -uthr 4 -bin BIGGEST/seg_FS_POSTPAR_${LR}
+	fslmaths BIGGEST/seg_all_${LR} -thr 5 -uthr 5 -bin BIGGEST/seg_FS_OCC_${LR}
+	fslmaths BIGGEST/seg_all_${LR} -thr 6 -uthr 6 -bin BIGGEST/seg_FS_TEMP_${LR}
+
+done
+
+# Combine left and right
+fslmaths BIGGEST/seg_all_L -add BIGGEST/seg_all_R BIGGEST/seg_all_LR
+
+# Make label list for segmentations
+cat > BIGGEST/seg_all-labels.csv <<HERE
+1,FS_PFC
+2,FS_MOTOR
+3,FS_SOMATO
+4,FS_POSTPAR
+5,FS_OCC
+6,FS_TEMP
+HERE
+
