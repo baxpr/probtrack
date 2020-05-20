133a5500ac0a3909bc6135895a5c8b5c724f8c30
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-20 17:29:04 -0500

    check coreg

--- src/make_pdf.sh
+++ src/make_pdf.sh
@@ -7,12 +7,12 @@ echo Running ${0}
 wkdir="${out_dir}"/makepdf
 mkdir "${wkdir}"
 
-# Coreg verification - outline of cortex ROI on coregistered T1
+# Coreg verification - outline of FS cortex ROI on mean b=0 DWI
 fsleyes render --outfile "${wkdir}"/coreg.pdf \
 	--hideCursor \
 	--xzoom 1000 --yzoom 1000 --zzoom 1000 \
 	--size 1200 500 \
-	"${out_dir}"/norm_to_DWI \
+	"${out_dir}"/b0_mean \
 	"${rois_dwi_dir}"/FS_CORTEX \
 		--overlayType label --outline --outlineWidth 2 --lut harvard-oxford-subcortical
 
