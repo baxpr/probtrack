bd506c42e06e8be5aad80cf1c1c7b8742e4fffac
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-20 12:39:51 -0500

    tweaks

--- src/make_pdf.sh
+++ src/make_pdf.sh
@@ -11,7 +11,7 @@ mkdir "${wkdir}"
 fsleyes render --outfile "${wkdir}"/coreg.pdf \
 	--hideCursor \
 	--xzoom 1000 --yzoom 1000 --zzoom 1000 \
-	--size 1200 600 \
+	--size 1200 500 \
 	"${out_dir}"/norm_to_DWI \
 	"${rois_dwi_dir}"/FS_CORTEX \
 		--overlayType label --outline --outlineWidth 2 --lut harvard-oxford-subcortical
