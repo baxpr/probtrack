a81aadcad92cf4d2a80f04f379a30706e43bb830
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-21 18:28:21 -0500

    warp

--- src/warp.sh
+++ src/warp.sh
@@ -5,11 +5,10 @@
 
 towarp_dir="${out_dir}"/PROBTRACK_FS6/FS_THALAMUS_L_to_FS_PFC_L
 
-# Affine to FS space with DWI_to_FS.mat
+# Affine to FS space with DWI_to_FS.mat, then SPM12 warp
 cd "${towarp_dir}"
 flirtopts="-applyxfm -init ${out_dir}/DWI_to_FS.mat -paddingsize 0.0 -interp nearestneighbour -ref ${out_dir}/norm.nii.gz"
 for f in *.nii.gz ; do
 	flirt ${flirtopts} -in "${f}" -out "r${f}"
+	"${matlab_dir}"/run_spm12.sh "${mcr_dir}" function warp "${out_dir}"/iy_invdef.nii "${towarp_dir}/${f}"
 done
-
-# FS to MNI via SPM12 deformation
