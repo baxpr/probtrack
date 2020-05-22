39b723383cb960d9ea31004349c0a429fce59ab3
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-21 19:08:41 -0500

    warptest

---
+++ src/test_warp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+out_dir=/wkdir/src/testdir/OUTPUTS
+
+/wkdir/matlab/bin/run_spm12.sh /usr/local/MATLAB/MATLAB_Runtime/v92 function warp ${out_dir}/PROBTRACK_FS6/FS_THALAMUS_L_to_FS_MOTOR_L
+
