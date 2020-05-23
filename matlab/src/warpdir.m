56340f5a37a80e7927516268e300d4c49dd32cad
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-23 18:11:41 -0500

    warpdir fix

--- matlab/src/warpdir.m
+++ matlab/src/warpdir.m
@@ -4,9 +4,8 @@ gzs = dir(fullfile(warp_dir,'r*.nii.gz'));
 gzs = cellstr(char(gzs.name));
 
 for g = 1:numel(gzs)
-	system(['cd ' warp_dir ' && gunzip ' gzs{g}]);
+	system(['gunzip ' fullfile(warp_dir,gzs{g})]);
 	towarpnii = gzs{g}(1:end-3);
 	warp(fullfile(out_dir,'y_fwddef.nii'),fullfile(warp_dir,towarpnii));
 	system(['gzip -f ' fullfile(warp_dir,['w' towarpnii])]);
 end
-
