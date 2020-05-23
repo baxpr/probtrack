b88b602e78a3de59b360cf5293e6fe6283c1c9f0
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-23 13:50:43 -0500

    probmaps working

--- src/do_probmaps.sh
+++ src/do_probmaps.sh
@@ -2,25 +2,25 @@
 #
 # Compute probmaps with proj_thresh
 
+# FIXME implement for INDIV and MULTI?
+
 # Info output
 echo "Running ${0}"
 echo "   Sources: ${source_regions}"
 echo "   Targets: ${target_regions}"
 echo "   Dir:     ${track_dir}"
 
-# Process for each source and hemisphere
+# Process for each source and hemisphere. proj_thresh is very picky
+# about how filenames are specified - we have a wildcard that should
+# find all the seeds_to files but exclude the _lengths files. And
+# it should be run from the seed files directory.
 for source in ${source_regions} ; do
 	for LR in L R ; do
 
-		# Get seed filenames for this multi-probtrack
-		seed_dir="${track_dir}/${source}_to_TARGETS_${LR}"
-		seed_files=""
-		for target in ${target_regions} ; do
-			seed_files="${seed_files} ${seed_dir}/seeds_to_${target}_${LR}"
-		done
-
+		seed_dir="${track_dir}/${source}_${LR}_to_TARGETS_${LR}"
 		cd "${seed_dir}"
-		proj_thresh "${seed_files}" 0
-
+		pwd
+		proj_thresh seeds_to_*_${LR}.nii.gz 0 > proj_thresh.log
+		
 	done
 done	
