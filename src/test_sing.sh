4d3d60265c4fb0fc8ba94045198338eea0596a58
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-24 15:40:38 -0500

    testscript

--- src/test_sing.sh
+++ src/test_sing.sh
@@ -5,15 +5,15 @@ singularity run \
 	--home $(pwd) \
 	--bind $(pwd)/INPUTS:/INPUTS \
 	--bind $(pwd)/OUTPUTS:/OUTPUTS \
-	baxpr-thaltrack-whole-master-latest.simg \
+	baxpr-thaltrack-whole-master-v1.0.0.simg \
 	--fs_subject_dir /INPUTS/SUBJECT \
 	--fs_nii_thalamus_niigz /INPUTS/ThalamicNuclei.v10.T1.FSvoxelSpace.nii.gz \
 	--b0mean_niigz /INPUTS/b0_mean.nii.gz \
 	--invdef_niigz /INPUTS/iy_t1.nii.gz \
+	--fwddef_niigz /INPUTS/y_t1.nii.gz \
 	--bedpost_dir /INPUTS/BEDPOSTX \
 	--probtrack_samples 100 \
 	--project TESTPROJ \
 	--subject TESTSUBJ \
 	--session TESTSESS \
 	--outdir /OUTPUTS
-
