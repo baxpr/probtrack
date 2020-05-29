8aca7f1f727423fc7a0936a09e3e27216908b6e5
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-29 17:50:37 -0500

    testscr

--- src/test_sing.sh
+++ src/test_sing.sh
@@ -6,7 +6,7 @@ singularity run \
 	--bind $(pwd)/INPUTS:/INPUTS \
 	--bind $(pwd)/OUTPUTS:/OUTPUTS \
 	--bind $(pwd)/freesurfer_license.txt:/usr/local/freesurfer/.license \
-	baxpr-thaltrack-whole-master-v2.0.0.simg \
+	baxpr-thaltrack-whole-master-v2.0.1.simg \
 	--fs_subject_dir /INPUTS/SUBJECT \
 	--fs_nii_thalamus_niigz /INPUTS/ThalamicNuclei.v10.T1.FSvoxelSpace.nii.gz \
 	--b0mean_niigz /INPUTS/b0_mean.nii.gz \
